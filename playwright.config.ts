1af7b85cc1c5449c2262dc732e5a6e0872846a78
Viacheslav Borysovskyi <viacheslavborysovskyi@Viacheslavs-MacBook-Pro.local>
2026-09-15 17:57:41 +0200

    change name test add note ui

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || 'https://practice.expandtesting.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     headless: true,
--- tests/ui/add-note-ui.spec.ts
+++ tests/ui/add-note-ui.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../../fixtures/baseFixture';
 
-test('User can add note',
+test('User can add a note',
   { tag: ['@smoke', '@ui'] },
   async ({ homePage, notesAppPage, loginPage }) => {
     await homePage.open();
